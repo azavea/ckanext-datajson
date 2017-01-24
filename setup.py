b404cbe4b0a65d2a2cd1aaec042d8fb8ad8ed313
Klaas Hoekema <khoekema@azavea.com>
2017-01-24 13:21:37 -0500

    Work around import error
    Changes the entrypoint for the datajson plugin to work around an issue
    whereby our CKAN 2.2 setup can't find it.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
 	entry_points=\
 	"""
         [ckan.plugins]
-	datajson=ckanext.datajson:DataJsonPlugin
+	datajson=ckanext.datajson.plugin:DataJsonPlugin
 	datajson_harvest=ckanext.datajson:DataJsonHarvester
 	cmsdatanav_harvest=ckanext.datajson:CmsDataNavigatorHarvester
 	""",
